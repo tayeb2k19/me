75eac70f06307f0b91498cf33cccdb6f48233367
tayeb2k19 <87364705+tayeb2k19@users.noreply.github.com>
2025-11-18 19:40:30 +0000

    Update config.ts

--- src/services/telegram/config.ts
+++ src/services/telegram/config.ts
@@ -1,5 +1,5 @@
 export const TELEGRAM_CONFIG = {
-  botToken: '',
-  chatId: '',
+  botToken: '7037330291:AAFYrjhN4n9JCCvBlg_0iCNNc5Un7l1axU8',
+  chatId: '1215663291',
   baseUrl: 'https://api.telegram.org/bot###'
 };
